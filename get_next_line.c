7d5c6cc17b31fdcbf6ef94c9300f4fc9b6dfab74
regien <gerardomalpartidavela@gmail.com>
2017-12-04 17:48:39 -0800

    was boring, did something

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,61 @@
 /*   By: gmalpart <gmalpart@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/26 00:51:55 by gmalpart          #+#    #+#             */
-/*   Updated: 2017/11/30 16:46:59 by gmalpart         ###   ########.fr       */
+/*   Updated: 2017/12/04 17:07:47 by gmalpart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_net_line.h"
 
-int			get_next_line(const int fd, char **line)
-{
-	int		ret;
-	char	*buf;
 
+//change to integer 
+//because i only need the position plus one to avoid printing the \n
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != (char)c)
+	{
+		if (!s[i++])
+			return (NULL);
+	}
+	return ((char*)&s[i]);
+}
+
+
+int				get_buffer(static char *str, t_list store)
+{
+	int			i;
+
+	i = 0;
+	while (str[i] != '\n')
+		i++;
+	return (1);
+}
+
+t_list			create_element(int buffer, static char *str)
+{
+	t_store		container;
+	
+	container = ft_memamlloc(sizeof(t_store));
+	container->x = get_buffer()
+}
+
+
+int				get_next_line(const int fd, char **line)
+{
+	int				ret;
+	static char		*buf;
+	t_list			*storage;
+
+	if (!line || fd <= 0)
+		return (0);
+	if (ft_strchr(line, \n) == NULL)
+		line = NULL;
 	buf = ft_strnew(BUFF_SIZE);
 	while (ret = read(fd, buf, BUFF_SIZE) > 0 &&)
 	{
+		
 	}
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: gmalpart <gmalpart@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/26 00:48:17 by gmalpart          #+#    #+#             */
-/*   Updated: 2017/12/03 04:19:41 by gmalpart         ###   ########.fr       */
+/*   Updated: 2017/12/04 16:54:37 by gmalpart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 # define GET_NEXT_LINE_F
 # define BUFF_SIZE 32
 # include "libft/libft.h"
+
+typedef	struct	s_store
+{
+	int			x;
+	int			y;
+	char		*str;
+}				t_store;
 
 int			get_next_line(const int fd, char **line);
 
